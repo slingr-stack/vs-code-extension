6077fed8c70b1a16b5d42a135f0dde2bb19467eb
Diego Gaviola <dgaviola@slingr.io>
2025-08-08 00:01:18 -0300

    removing old things in the repo

--- option-interfaces-references/framework/metadata/entity.ts
+++
@@ -1,7 +0,0 @@
-import { Field } from "./field";
-
-export interface Entity {
-    label: string;
-    name: string;
-    fields: Field[];
-}
--- option-interfaces/utils.ts
+++
@@ -1,5 +0,0 @@
-export function formatDateTime(dateTime: Date, format: string) {
-    // Format the given date using the pattern in `format`
-    // TODO
-    return format;
-}
