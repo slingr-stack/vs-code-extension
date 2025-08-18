4ac5d57fb6a97acaba37909c3c1867f8f6b74546
ElPelado619 <agustinyornet@gmail.com>
2025-08-18 12:43:12 -0300

    initialize TypeScript project with sum function and tests

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: { "^.+\\.tsx?$": ["ts-jest", {"rootDir": "."}] }
+};
